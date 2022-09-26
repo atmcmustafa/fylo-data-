0d4d2c60b3e715e16ff0cfd6bd588a53dd64bb87
atmcmustafa <mustafa_atmaca57@hotmail.com>
2022-09-26 13:02:00 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      raleway: 'Raleway',
+      
+    },
+    colors: {
+     gradient :' hsl(6, 100%, 80%) to hsl(335, 100%, 65%)',
+      paleBlue: 'hsl(243, 100%, 93%)',
+      grayishBlue: ' hsl(229, 7%, 55%)',
+      darkBlue: 'hsl(228, 56%, 26%)',
+      veryDarkBlue: 'hsl(229, 57%, 11%)',
+
+    },
+
+    container: {
+      center: 'true',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
